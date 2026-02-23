539bbaddd6a3a24a5a68c42ed3fb6b141f5a845d
safonovdmitrij <safonovdmitrij2006@gmail.com>
2026-02-23 12:28:53 +0200

    lab 2: single-threaded baseline implementation

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <ctime>
 
-const int size = 100;
+constexpr size_t size = 5000000;
 
-void fill_array(std::vector<int>& array, int size)
+void fillArray(std::vector<int>& array, size_t size)
 {
     for(int i = 0; i < size; i++)
     {
-        array[i] = rand() % 1000;
+        array[i] = rand() % (10000 - 10 + 1) + 10;
     }
 }
 
-void print_array(std::vector<int>& array, int size)
+void printArray(std::vector<int>& array, size_t size)
 {
-    for(int i = 0; i < size; i++)
+    for(size_t i = 0; i < size; i++)
     {
         std::cout << array[i] << " ";
     }
 }
 
-void processArray(std::vector<int>& array, int size, int& minimum, int& globalSum)
+void processArray(std::vector<int>& array, size_t size, int& minimum, long long& globalSum)
 {
-    for(int i = 0; i < size; i++)
+    for(size_t i = 0; i < size; i++)
     {
         if(array[i] % 10 == 0)
         {
@@ -38,17 +42,23 @@ void processArray(std::vector<int>& array, int size, int& minimum, int& globalSu
 
 int main()
 {
+    srand(123);
     std::vector<int> array(size);
-    int globalSum = 0;
-    int minimum = 10000;
+    long long globalSum = 0;
+    int minimum = INT_MAX;
 
-    fill_array(array, size);
-    print_array(array, size);
+    fillArray(array, size);
+
+
+    auto startTime = std::chrono::high_resolution_clock::now();
 
     processArray(array, size, minimum, globalSum);
 
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+
     std::cout << "\nGlobal Sum: " << globalSum << std::endl;
-    if(minimum == 10000)
+    if(minimum == INT_MAX)
     {
         std::cout << "No elements divisible by 10 found\n";
     }
@@ -56,6 +66,7 @@ int main()
     {
         std::cout << "Minimum: " << minimum << std::endl;
     }
+    std::cout << "Total time: " << duration.count() << " microseconds\n";
 
     return 0;
 }
